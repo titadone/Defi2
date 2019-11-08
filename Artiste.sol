b8b213623decf699bff394715510f91edff47c79
titadone <bloum93200@gmail.com>
2019-11-08 11:31:43 +0100

    2

---
+++ Artiste.sol
@@ -0,0 +1,45 @@
+pragma solidity ^0.5.11;
+//pragma experimental ABIEncoderV2;
+
+contract ArtisteContract{
+
+    struct Artiste{
+        string nom;
+        uint256 reputation;
+    }
+
+    mapping(address => Artiste) list_artiste;
+    address[] list_ban;
+    address owner;
+
+    constructor(){
+        owner = msg.sender;
+    }
+
+    modifier admin(){
+        require(msg.sender == owner);
+    }
+
+    function inscription(string memory _nom) public{
+        require(contains(msg.sender) == true , "Vous êtes déjà banni!");
+
+        list_artiste[msg.sender].reputation = 1;
+        list_artiste[msg.sender].nom = _nom;
+    }
+
+    function ban(address ban_user) public admin{
+        list_artiste[ban_user].reputation = 0;
+        list_ban.push(ban_user);
+    }
+
+    function contains(address is_ban) public (bool){
+        bool find = false;
+
+        for(uint i = 0; list_ban.length; i++){
+            if(list_ban[i] == is_ban){
+                find = true;
+            }
+        }
+        return find;
+    }
+}
