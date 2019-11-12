b0ab062b10abe29f8871e5b70c9aad1c5ea976fd
TonySeperoumal <tony.seperoumal@gmail.com>
2019-11-12 09:37:10 +0100

    v2 mapping list_ban

--- marketPlace.sol
+++ marketPlace.sol
@@ -2,11 +2,9 @@ pragma solidity ^0.5.11
 
 contract marketPlace{
   mapping(address => User) private users;
-  address[] list_ban;
+  mapping(address => bool) list_ban;
   address owner;
 
-  //mapping(address => string) private username;
-  //uint reputation;
   modifier admin(){
     require(msg.sender == owner);
   }
@@ -21,25 +19,15 @@ contract marketPlace{
   }
 
   function  inscripton(string memory _nom) public{
-      //users[msg.sender];
-      //usernames[msg.sender];
-      //reputation += 1;
-      require(contain(msg.sender) == true, "Vous êtes déjà banni !");
+      require(list_ban[msg.sender] == true, "Vous êtes déjà banni !");
       users[msg.sender].reputation += 1;
       users[msg.sender].nom = _nom;
   }
 
   function ban(address ban_user) public admin{
     users[ban_user].reputaion = 0;
-    list_ban.push[ban_user];
+    list_ban[ban_user] = true;
   }
 
-  function contain(address is_ban) public view returns(bool){
-      bool find = false;
-    for(uint i = 0; i < list_ban.length; i++){
-        if(list_ban[i] == is_ban){
-            return true;
-        }
-        return find;
-    }
+
 }
