44c752c8909ad5df8fd525bbb787fd677cf77cef
titadone <bloum93200@gmail.com>
2019-11-15 15:32:36 +0100

    update Artiste.sol

--- IOffre.sol
+++ IOffre.sol
@@ -3,9 +3,11 @@ pragma solidity ^0.5.11;
 
 interface IOffre{
 
-    function listOffres()external;
+    function listOffres() external;
 
     function getOffre(address addresse) external;
 
     function faireOffre(address) external;
+
+    function annulerOffre(address) external;
 }
