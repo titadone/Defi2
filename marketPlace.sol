aaf28f7bd62a35fe8f5d8bc373bc91d0d7f4f58b
TonySeperoumal <tony.seperoumal@gmail.com>
2019-11-15 10:08:46 +0100

    v1

--- marketPlace.sol
+++ marketPlace.sol
@@ -1,8 +1,10 @@
-pragma solidity ^0.5.11
+pragma solidity ^0.5.11;
 
 contract marketPlace{
   mapping(address => User) private users;
   mapping(address => bool) list_ban;
+  mapping(address => Demande) candidats;
+  Demande[] list_demande;
   address owner;
 
   modifier admin(){
@@ -12,6 +14,17 @@ contract marketPlace{
   struct User {
     uint reputation;
     string nom;
+  }
+
+  enum etatDemande {OUVERTE, EN COURS, FERMEE}
+
+  struct Demande {
+    uint256 remuneration;
+    uint delaiAcceptation;
+    string description;
+    etatDemande statut;
+    uint minReputation;
+    uint candidat;
   }
 
   constructor() {
@@ -27,6 +40,14 @@ contract marketPlace{
   function ban(address ban_user) public admin{
     users[ban_user].reputaion = 0;
     list_ban[ban_user] = true;
+  }
+
+  function ajouterDemande(uint256 remuneration, string memory description, uint delaiAcceptation ) public payable{
+    require(msg.value == remuneration + ((remuneration * 2) / 100), "N \'oubliez pas les 2% de frais !");
+    nouvelleDemande.remenuration = _remuneration;
+    nouvelleDemande.description = _description;
+    nouvelleDemande.statut = etatDemande[0];
+
   }
 
 
